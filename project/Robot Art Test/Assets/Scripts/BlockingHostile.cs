9f34299769ff0f4671b088df8a1ae7f458fc86b3
retrigar <42656578+retrigar@users.noreply.github.com>
2020-04-11 20:27:30 -0400

    4.11 backup

---
+++ project/Robot Art Test/Assets/Scripts/BlockingHostile.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BlockingHostile : MonoBehaviour
+{
+    public Transform fallback;
+    public Transform fallback2;
+    public Transform finalpoint;
+    public Transform PlayerTarget;
+    public bool moved = false;
+    public bool fellback = false;
+    public bool playerseen;
+    NavMeshAgent nav;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerseen == true)
+        {
+            Player = GameObject.Find("player_stand_in");
+            PlayerTarget = Player.transform;
+            Vector3 lookVector = Player.transform.position - transform.position;
+            //lookVector.y = Player.transform.position.y;
+            Quaternion rot = Quaternion.LookRotation(lookVector);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+            nav.SetDestination(fallback.position);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //player takes damage
+        }
+        if (other.gameObject.tag == "D1")
+        {
+            moved = true;
+        }
+        if (other.gameObject.tag == "D2")
+        {
+            fellback = true;
+        }
+        if (other.gameObject.tag == "projectile")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && playerseen == false)
+        {
+            playerseen = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        /*if (other.gameObject.tag == "Player" && playerseen == false)
+        {
+            playerseen = true;
+        }*/
+        if (playerseen == true)
+        {
+            if (other.gameObject.tag == "Player" && moved == false)
+            {
+                nav.SetDestination(fallback.position);
+                moved = true;
+            }
+        }
+    }
+}
---
+++ project/Robot Art Test/Assets/Scripts/HostileScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HostileScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "projectile")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }*/
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "projectile")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
