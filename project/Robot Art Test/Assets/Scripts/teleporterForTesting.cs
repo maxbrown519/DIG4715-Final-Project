8b013ae9d123d534383d239c6df57374bbb3d582
retrigar <42656578+retrigar@users.noreply.github.com>
2020-04-13 14:51:23 -0400

    2:51 update

---
+++ project/Robot Art Test/Assets/Scripts/teleporterForTesting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleporterForTesting : MonoBehaviour
+{
+    private GameObject player;
+    public Transform NextLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.position = NextLocation.transform.position;
+        }
+    }
+}
