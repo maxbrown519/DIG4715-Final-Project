3f89a960b238620c408e4338e7929b9d1f027c3b
LexineAce <46612250+LexineAce@users.noreply.github.com>
2020-04-20 21:05:32 -0400

    octoPlat

---
+++ Robot Art Test/Assets/Scripts/projectileScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this.gameObject, 4);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(this.gameObject);
+        if (other.gameObject.tag == "hostile")
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+    /*private void OncTriggerEnter(Collision other)
+    {
+        Destroy(this.gameObject);
+        if (other.gameObject.tag == "hostile")
+        {
+            Destroy(other.gameObject);
+        }
+    }*/
+    /*private void OnTriggerEnter(Collider other)
+    {
+        // Destroy(this.gameObject);
+        if (other.gameObject.tag == "hostile")
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+    }*/
+}
