1d304830905fa55b86fa7dfde4e35e30add22c1d
retrigar <42656578+retrigar@users.noreply.github.com>
2020-04-26 19:51:44 -0400

    volume control semi-functional

--- Robot Art Test/Assets/Scripts/dontdestrod.cs
+++ Robot Art Test/Assets/Scripts/dontdestrod.cs
@@ -6,17 +6,25 @@ using UnityEngine.UI;
 public class dontdestrod : MonoBehaviour
 {
     public static GameObject Instance;
+    private GameObject hardText;
     public Text HardModeText;
+    private GameObject invText;
     public Text InvincibilityText;
     [SerializeField] public float Invincibility;
     [SerializeField] public float HardMode;
     private float ActiveInvincivility;
     private float ActiveHardMode;
+    public GameObject VolSlider;
+    public float Volume;
     
     private void Awake()
     {
             DontDestroyOnLoad(this.gameObject);
             Instance = this.gameObject;
+        hardText = GameObject.Find("hard mode");
+        HardModeText = hardText.GetComponent<Text>();
+        invText = GameObject.Find("invinsability");
+        InvincibilityText = invText.GetComponent<Text>();
     }
     private void LateUpdate()
     {
@@ -30,13 +38,13 @@ public class dontdestrod : MonoBehaviour
     public void InvincibilityMode()
     {
         Invincibility = 1;
-        HardModeText.text = "Invincibility Mode Enabled";
+        InvincibilityText.text = "Invincibility Mode Enabled";
         HardMode = 0;
     }
     public void InvincibilityModeOff()
     {
         Invincibility = 0;
-        
+        InvincibilityText.text = "";
     }
     public void DifficultyMode()
     {
@@ -48,6 +56,13 @@ public class dontdestrod : MonoBehaviour
     public void DifficultyModeoff()
     {
         HardMode = 0;
+        HardModeText.text = "";
+    }
+
+    public void ChangeVol()
+    {
+        Volume = GameObject.Find("Volume Slider").GetComponent<Slider>().value;
+
     }
 }
 
